1b9c4e4d1654a0353ec542d86f76389a28772531
7LevelLabs <alex.velichko.kyiv@gmail.com>
2014-12-26 09:47:36 +0200

    BF

--- rs/src/test/java/ua/ll7/slot7/ma/service/impl/UserServiceIT.java
+++ rs/src/test/java/ua/ll7/slot7/ma/service/impl/UserServiceIT.java
@@ -43,33 +43,14 @@ public class UserServiceIT extends Assert {
 	}
 
 	@Test
-	@Ignore
 	public void testUpdateUser() {
 		User user = MAFactory.getNewUser("email", "nick", "name", "password");
 		userService.save(user);
 
-		CategoryForTheUser categoryForTheUser1 = MAFactory.getNewCategory(user, "Cat1", "Category1");
-		CategoryForTheUser categoryForTheUser2 = MAFactory.getNewCategory(user, "Cat2", "Category2");
-
-		categoryService.save(categoryForTheUser1);
-		categoryService.save(categoryForTheUser2);
-
-		userService.save(user);
-
-		System.out.println(user);
+		user.setApiCode("New API");
 
 		User userRead = userService.findByEMail("email");
 
-		List<CategoryForTheUser> categoryForTheUserSet = userRead.getCategories();
-
-		CategoryForTheUser categoryForTheUser3 = new CategoryForTheUser();
-		categoryForTheUser3.setUser(user);
-		categoryForTheUser3.setName("Cat3");
-
-		org.assertj.core.api.Assertions.assertThat(categoryForTheUserSet)
-			.isNotEmpty()
-			.hasSize(2)
-			.contains(categoryForTheUser1, categoryForTheUser2)
-			.doesNotContain(categoryForTheUser3);
+		assertEquals(userRead.getApiCode(), "New API");
 	}
 }
