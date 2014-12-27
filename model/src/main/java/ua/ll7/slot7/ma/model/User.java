8c25d4885baa1cdcc1a3a283fd7d318c2abb3cc6
7LevelLabs <alex.velichko.kyiv@gmail.com>
2014-12-27 16:13:28 +0200

    notNull accuracy

--- model/src/main/java/ua/ll7/slot7/ma/model/User.java
+++ model/src/main/java/ua/ll7/slot7/ma/model/User.java
@@ -34,12 +34,10 @@ public class User implements Serializable {
   @Column(nullable = false)
   private String password;
 
-  @NotBlank(message = "User's Nick must be not blank")
-  @Column(nullable = false)
+  @Column
   private String nick;
 
-  @NotBlank(message = "User's Name must be not blank")
-  @Column(nullable = false)
+  @Column
   private String name;
 
   @Column(nullable = false)
