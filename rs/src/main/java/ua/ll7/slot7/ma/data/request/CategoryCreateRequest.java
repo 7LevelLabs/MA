8a8c21bbff90cd52784ea586fefffd971afeb6a5
7LevelLabs <alex.velichko.kyiv@gmail.com>
2014-12-29 23:43:04 +0200

    API: Category - Create

--- rs/src/main/java/ua/ll7/slot7/ma/data/request/CategoryCreateRequest.java
+++ rs/src/main/java/ua/ll7/slot7/ma/data/request/CategoryCreateRequest.java
@@ -1,11 +1,11 @@
 package ua.ll7.slot7.ma.data.request;
 
-import ua.ll7.slot7.ma.data.generic.StringLongDG;
+import ua.ll7.slot7.ma.data.generic.StringDG;
 
 /**
  * @author Alex Velichko
  *         10.06.14 : 15:50
  */
-public class CategoryCreateRequest extends StringLongDG {
+public class CategoryCreateRequest extends StringDG {
 
 }
