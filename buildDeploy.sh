d7c088ce879c2a3498ab3c1dc824c8c720f7a9da
7LevelLabs <alex.velichko.kyiv@gmail.com>
2015-01-12 18:42:09 +0200

    script accuracy

--- buildDeploy.sh
+++ buildDeploy.sh
@@ -5,10 +5,12 @@ artefactName="ll7ma-rs.war"
 
 case $target in
     dev)
-    mvn -Djetty.port=9997 clean package -P dev -e
+    mvn clean package site -P dev -e
     ;;
 esac
 
 echo "Deploy..."
 
 cp $artefactLocation/$artefactName $CATALINA_BASE/webapps
+
+echo "Done."
