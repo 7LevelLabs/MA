e2f86d214cf5fe478ca67aa9977eecfd3129d8f0
7LevelLabs <alex.velichko.kyiv@gmail.com>
2015-01-21 17:43:46 +0200

    Tests improvements

--- rs/src/test/java/ua/ll7/slot7/ma/service/impl/BLServiceImplTest.java
+++ rs/src/test/java/ua/ll7/slot7/ma/service/impl/BLServiceImplTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+import ua.ll7.slot7.ma.data.request.CategoryUpdateRequest;
 import ua.ll7.slot7.ma.data.request.UserListPageableRequest;
 import ua.ll7.slot7.ma.data.request.UserRegisterRequest;
 import ua.ll7.slot7.ma.data.request.UserSetActiveRequest;
@@ -61,8 +62,8 @@ public class BLServiceImplTest extends Assert {
     List<UserVO> userVOs = blService.userList();
 
     org.assertj.core.api.Assertions.assertThat(userVOs)
-                         .isNotEmpty()
-                         .hasSize(2);
+                                   .isNotEmpty()
+                                   .hasSize(2);
 
   }
 
@@ -87,8 +88,8 @@ public class BLServiceImplTest extends Assert {
     List<UserVO> userVOs = blService.userListPageable(request);
 
     org.assertj.core.api.Assertions.assertThat(userVOs)
-                         .isNotEmpty()
-                         .hasSize(3);
+                                   .isNotEmpty()
+                                   .hasSize(3);
 
     request.setData1(2);
     request.setData2(3);
@@ -96,8 +97,8 @@ public class BLServiceImplTest extends Assert {
     userVOs = blService.userListPageable(request);
 
     org.assertj.core.api.Assertions.assertThat(userVOs)
-                         .isNotEmpty()
-                         .hasSize(1);
+                                   .isNotEmpty()
+                                   .hasSize(1);
   }
 
   @Test
@@ -126,6 +127,28 @@ public class BLServiceImplTest extends Assert {
   }
 
   @Test
+  public void testCategoryUpdate() throws Exception {
+    User user = MAFactory.getNewUserForTestsFS("email", "nick", "name", "password");
+    userService.save(user);
+
+    blService.categoryCreateForUser(user, "Cat1", "Category1");
+
+    CategoryUpdateRequest request = new CategoryUpdateRequest();
+    request.setData1("Cat1 - new");
+    request.setData2("Category1 - new");
+    request.setData3(user.getCategories().get(0).getId());
+
+    blService.categoryUpdate(request);
+
+    User userRead = userService.findByEMail("email");
+    CategoryForTheUser category = userRead.getCategories().get(0);
+
+    assertEquals(category.getName(), "Cat1 - new");
+    assertEquals(category.getDescription(), "Category1 - new");
+
+  }
+
+  @Test
   public void testExpenseCreateForCategory() throws Exception {
     User user = MAFactory.getNewUserForTestsFS("email", "nick", "name", "password");
     userService.save(user);
@@ -135,7 +158,7 @@ public class BLServiceImplTest extends Assert {
     Expense expense = blService.expenseCreateForCategory(categoryForTheUser, CurrencyUnit.USD, 10.01F, "10-12-2015");
 
     assertTrue(expense.getCategoryForTheUser().getName().equals("Cat3") &&
-                 expense.getCategoryForTheUser().getId() > 0);
+                      expense.getCategoryForTheUser().getId() > 0);
   }
 
   @Test
@@ -157,7 +180,6 @@ public class BLServiceImplTest extends Assert {
     ;
   }
 
-  @Test
   public void testUserSetActive() throws Exception {
     User user = MAFactory.getNewUserForTestsFS("email", "nick", "name", "password");
     userService.save(user);
@@ -181,7 +203,5 @@ public class BLServiceImplTest extends Assert {
     assertEquals(user.getRole(), 0);
 
   }
-
-
 
 }
